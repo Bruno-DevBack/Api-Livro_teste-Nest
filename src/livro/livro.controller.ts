a6999e1f86063dc10b3d6b194f073542936a985c
Bruno-DevBack <fonsecabruno6062@gmailcom>
2025-05-20 19:41:39 -0300

    outros metodos http

--- src/livro/livro.controller.ts
+++ src/livro/livro.controller.ts
@@ -1,10 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete, Param, Put } from '@nestjs/common';
 import { LivroService } from './livro.service';
+import { Livro } from './livro.entity';
+
 @Controller('livro')
 export class LivroController {
- constructor(private readonly livroService: LivroService) {}
- @Get()
- findAll() {
- return this.livroService.findAll();
- }
+  constructor(private readonly livroService: LivroService) {}
+
+  @Get()
+  findAll() {
+    return this.livroService.findAll();
+  }
+
+  @Post()
+  create(@Body() livro: Livro) {
+    return this.livroService.create(livro);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.livroService.remove(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() livro: Livro) {
+    return this.livroService.update(id, livro);
+  }
 }
--- src/livro/livro.service.ts
+++ src/livro/livro.service.ts
@@ -1,14 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Livro } from './livro.entity';
+
 @Injectable()
 export class LivroService {
  constructor(
  @InjectRepository(Livro)
  private livroRepository: Repository<Livro>,
  ) {}
+
  findAll(): Promise<Livro[]> {
  return this.livroRepository.find();
+ }
+
+ create(livro: Livro): Promise<Livro> {
+ return this.livroRepository.save(livro);
+ }
+
+ async remove(id: number): Promise<void> {
+ await this.livroRepository.delete(id);
+ }
+
+ async update(id: number, livro: Livro): Promise<Livro> {
+ await this.livroRepository.update(id, livro);
+ const updatedLivro = await this.livroRepository.findOne({ where: { id } });
+ if (!updatedLivro) {
+ throw new NotFoundException(`Livro com ID ${id} não encontrado`);
+ }
+ return updatedLivro;
  }
 }
